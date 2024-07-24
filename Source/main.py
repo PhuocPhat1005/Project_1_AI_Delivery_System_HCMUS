751c50dd6c0d0c17115247ecb83ae38f404e05d8
thanhto24 <110890930+thanhto24@users.noreply.github.com>
2024-07-24 22:31:56 +0700

    add test level2

--- Source/main.py
+++ Source/main.py
@@ -31,6 +31,19 @@ def main():
     # Write paths to the output file
     write_paths_to_file("output/output1_level2.txt", vehicles)
 
+    #Test level 2
+    # board = Board(n, m, f, t, map_data, level=2)
+    # S_vehicle = board.vehicle[0]   
+    # paths = []     
+    # board.test_display_path(paths)
+    # print (paths)
+    # print(S_vehicle.process_lev3(board))
+    # paths.append(S_vehicle.process_lev2(board))
+    # if(paths != []):
+    #     board.test_display_path(paths)
+    # else:
+    #     print("Can't find path, lev 2")
+
     # Test level 3
     # board = Board(n, m, f, t, map_data, level=4)
     # S_vehicle = board.vehicle[0]
@@ -38,10 +51,11 @@ def main():
     # # print(paths)
     # paths.append(S_vehicle.process_lev3(board))
     # if paths != []:
-
     #     board.test_display_path(paths)
     # else:
     #     print("Can't find path, lev 3")
+
+    #Test level 4
     # board = Board(n, m, f, t, map_data, level=4)
     # paths = process_lev4(board)
     # print(paths)
